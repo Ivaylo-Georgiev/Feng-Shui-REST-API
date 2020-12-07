81573c0d1cd572bc210c2687c4fab950bb2df208
Ivaylo <ivaylo.georgiev18@gmail.com>
2020-12-07 15:19:23 +0200

    Create FengShuiRestApiApplication.java

---
+++ src/main/java/com/fmi/fengshuirestapi/FengShuiRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.fengshuirestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengShuiRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengShuiRestApiApplication.class, args);
+	}
+
+}
