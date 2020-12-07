b8c240d21297a82129a622d3c1b5239f3c32251f
Ivaylo <ivaylo.georgiev18@gmail.com>
2020-12-07 17:20:44 +0200

    fix kua calculation after 2000

--- src/main/java/com/fmi/fengshuirestapi/util/KuaUtil.java
+++ src/main/java/com/fmi/fengshuirestapi/util/KuaUtil.java
@@ -10,13 +10,21 @@ public class KuaUtil {
 	private final static int KUA_TWO = 2;
 	private final static int FEMALE_KUA_ADDEND = 4;
 	private final static int MALE_KUA_MINUEND = 11;
+	private final static int MALE_KUA_MINUEND_AFTER_2000 = 9;
+	private final static int FEMALE_KUA_ADDEND_AFTER_2000 = 6;
 
 	private int yearLastTwoDigits;
 	private Gender gender;
+	private boolean isAfter2000;
 
 	public KuaUtil(int year, Gender gender) {
 		this.yearLastTwoDigits = year % LAST_TWO_DIGITS_DIVISOR;
 		this.gender = gender;
+		if (year >= 2000) {
+			this.isAfter2000 = true;
+		} else {
+			this.isAfter2000 = false;
+		}
 	}
 
 	public String execute() {
@@ -28,16 +36,24 @@ public class KuaUtil {
 
 	private int calculateMaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.yearLastTwoDigits);
-		int kuaNumber = MALE_KUA_MINUEND - reducedYear;
-		kuaNumber = reduceToSingleDigit(kuaNumber);
-		return (kuaNumber == KUA_FIVE) ? KUA_TWO : kuaNumber;
+		if (isAfter2000) {
+			return MALE_KUA_MINUEND_AFTER_2000 - reducedYear;
+		} else {
+			int kuaNumber = MALE_KUA_MINUEND - reducedYear;
+			kuaNumber = reduceToSingleDigit(kuaNumber);
+			return (kuaNumber == KUA_FIVE) ? KUA_TWO : kuaNumber;
+		}
 	}
 
 	private int calculateFemaleKuaNumber() {
 		int reducedYear = reduceToSingleDigit(this.yearLastTwoDigits);
-		int kuaNumber = FEMALE_KUA_ADDEND + reducedYear;
-		kuaNumber = reduceToSingleDigit(kuaNumber);
-		return (kuaNumber == KUA_FIVE) ? KUA_EIGHT : kuaNumber;
+		if (isAfter2000) {
+			return reduceToSingleDigit(FEMALE_KUA_ADDEND_AFTER_2000 + reducedYear);
+		} else {
+			int kuaNumber = FEMALE_KUA_ADDEND + reducedYear;
+			kuaNumber = reduceToSingleDigit(kuaNumber);
+			return (kuaNumber == KUA_FIVE) ? KUA_EIGHT : kuaNumber;
+		}
 	}
 
 	private int reduceToSingleDigit(int number) {
