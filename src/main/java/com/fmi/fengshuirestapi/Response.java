79a91974e594548518d8c074b21668aea0351927
Ivaylo <ivaylo.georgiev18@gmail.com>
2020-12-07 16:37:09 +0200

    add content type

--- src/main/java/com/fmi/fengshuirestapi/Response.java
+++ src/main/java/com/fmi/fengshuirestapi/Response.java
@@ -1,10 +1,16 @@
 package com.fmi.fengshuirestapi;
 
 public class Response {
+	private final String contentType;
 	private final String content;
 
-	public Response(String content) {
+	public Response(String contentType, String content) {
+		this.contentType = contentType;
 		this.content = content;
+	}
+
+	public String getContentType() {
+		return contentType;
 	}
 
 	public String getContent() {
