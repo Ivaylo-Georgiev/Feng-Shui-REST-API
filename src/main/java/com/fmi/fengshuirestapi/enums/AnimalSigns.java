555164d01f3885c1f57d50b558e7fa33a3c5c741
Ivaylo <ivaylo.georgiev18@gmail.com>
2020-12-10 22:33:23 +0200

    add peach blossom animals

--- src/main/java/com/fmi/fengshuirestapi/enums/AnimalSigns.java
+++ src/main/java/com/fmi/fengshuirestapi/enums/AnimalSigns.java
@@ -2,25 +2,30 @@ package com.fmi.fengshuirestapi.enums;
 
 public enum AnimalSign {
 
-	RAT(4, 23, "OX", "DRAGON,MONKEY", "HORSE"), OX(5, 1, "RAT", "SNAKE,ROOSTER", "SHEEP"), TIGER(6, 3, "PIG",
-			"HORSE,DOG", "MONKEY"), RABBIT(7, 5, "DOG", "SHEEP,PIG", "ROOSTER"), DRAGON(8, 7, "ROOSTER", "MONKEY,RAT",
-					"DOG"), SNAKE(9, 9, "MONKEY", "OX,ROOSTER", "PIG"), HORSE(10, 11, "SHEEP", "TIGER,DOG",
-							"RAT"), SHEEP(11, 13, "HORSE", "RABBIT,PIG", "OX"), MONKEY(0, 15, "SNAKE", "DRAGON,RAT",
-									"TIGER"), ROOSTER(1, 17, "DRAGON", "OX,SNAKE", "RABBIT"), DOG(2, 19, "RABBIT",
-											"TIGER,HORSE", "DRAGON"), PIG(3, 21, "TIGER", "RABBIT,SHEEP", "SNAKE");
+	RAT(4, 23, "OX", "DRAGON,MONKEY", "HORSE", "ROOSTER"), OX(5, 1, "RAT", "SNAKE,ROOSTER", "SHEEP", "HORSE"), TIGER(6,
+			3, "PIG", "HORSE,DOG", "MONKEY",
+			"RABBIT"), RABBIT(7, 5, "DOG", "SHEEP,PIG", "ROOSTER", "RAT"), DRAGON(8, 7, "ROOSTER", "MONKEY,RAT", "DOG",
+					"ROOSTER"), SNAKE(9, 9, "MONKEY", "OX,ROOSTER", "PIG", "HORSE"), HORSE(10, 11, "SHEEP", "TIGER,DOG",
+							"RAT", "RABBIT"), SHEEP(11, 13, "HORSE", "RABBIT,PIG", "OX", "RAT"), MONKEY(0, 15, "SNAKE",
+									"DRAGON,RAT", "TIGER", "ROOSTER"), ROOSTER(1, 17, "DRAGON", "OX,SNAKE", "RABBIT",
+											"HORSE"), DOG(2, 19, "RABBIT", "TIGER,HORSE", "DRAGON",
+													"RABBIT"), PIG(3, 21, "TIGER", "RABBIT,SHEEP", "SNAKE", "RAT");
 
 	private int yearModulo;
 	private int fromHour;
 	private String secretFriend;
 	private String astrologyAllies;
 	private String enemy;
+	private String peachBlossomAnimal;
 
-	private AnimalSign(int yearModulo, int fromHour, String secretFriend, String astrologyAllies, String enemy) {
+	private AnimalSign(int yearModulo, int fromHour, String secretFriend, String astrologyAllies, String enemy,
+			String peachBlossomAnimal) {
 		this.yearModulo = yearModulo;
 		this.fromHour = fromHour;
 		this.secretFriend = secretFriend;
 		this.astrologyAllies = astrologyAllies;
 		this.enemy = enemy;
+		this.peachBlossomAnimal = peachBlossomAnimal;
 	}
 
 	public static AnimalSign mapYearToSign(int year) {
@@ -57,6 +62,10 @@ public enum AnimalSign {
 
 	public String getEnemy() {
 		return this.enemy;
+	}
+
+	public String getPeachBlossomAnimal() {
+		return this.peachBlossomAnimal;
 	}
 
 }
