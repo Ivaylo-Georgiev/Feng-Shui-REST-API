5465ae3fe3c0e4c11c143c42760a901fda3a2c42
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2021-01-11 11:28:36 +0200

    add missing import

--- src/main/java/com/fmi/fengshuirestapi/app/FengShuiResponseController.java
+++ src/main/java/com/fmi/fengshuirestapi/app/FengShuiResponseController.java
@@ -1,5 +1,6 @@
 package com.fmi.fengshuirestapi.app;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
