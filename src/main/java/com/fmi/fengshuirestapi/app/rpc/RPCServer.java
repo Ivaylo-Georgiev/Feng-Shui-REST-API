24181f0f0509df7ecea3a517f048acacdf11bfbe
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2021-01-11 11:27:25 +0200

    move rpc logic in separate class

--- src/main/java/com/fmi/fengshuirestapi/app/rpc/RPCServer.java
+++ src/main/java/com/fmi/fengshuirestapi/app/rpc/RPCServer.java
@@ -1,5 +1,11 @@
-package com.fmi.fengshuirestapi.rpc;
+package com.fmi.fengshuirestapi.app.rpc;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.fmi.fengshuirestapi.pojo.RPCParameters;
+import com.fmi.fengshuirestapi.util.FengShuiDetailsUtil;
+import com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -9,26 +15,18 @@ import com.rabbitmq.client.DeliverCallback;
 public class RPCServer {
 
 	private static final String RPC_QUEUE_NAME = "rpc_queue";
+	private static final String HOST = "localhost";
+	private static final String ENCODING = "UTF-8";
 
-	private static int fib(int n) {
-		if (n == 0)
-			return 0;
-		if (n == 1)
-			return 1;
-		return fib(n - 1) + fib(n - 2);
-	}
-
-	public static void main(String[] argv) throws Exception {
+	public void establishRPCServerSide() throws IOException, TimeoutException {
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		factory.setHost(HOST);
 
 		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
 			channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
 			channel.queuePurge(RPC_QUEUE_NAME);
 
 			channel.basicQos(1);
-
-			System.out.println(" [x] Awaiting RPC requests");
 
 			Object monitor = new Object();
 			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
@@ -38,16 +36,15 @@ public class RPCServer {
 				String response = "";
 
 				try {
-					String message = new String(delivery.getBody(), "UTF-8");
-					int n = Integer.parseInt(message);
-
-					System.out.println(" [.] fib(" + message + ")");
-					response += fib(n);
+					String message = new String(delivery.getBody(), ENCODING);
+					RPCParameters params = new Gson().fromJson(message, RPCParameters.class);
+					response += FengShuiDetailsUtil.fengShuiDetails(params.getYear(), params.getHour(),
+							params.getGender());
 				} catch (RuntimeException e) {
 					System.out.println(" [.] " + e.toString());
 				} finally {
 					channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps,
-							response.getBytes("UTF-8"));
+							response.getBytes(ENCODING));
 					channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 					// RabbitMq consumer worker thread notifies the RPC server owner thread
 					synchronized (monitor) {
@@ -70,4 +67,5 @@ public class RPCServer {
 			}
 		}
 	}
+
 }
